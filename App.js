38bf8db3b10aa06f470739bb7551a18fe23afd28
adesesanayomide45-ctrl <adesesanayomide45@gmail.com>
2026-09-08 20:53:21 +0100

    Update App.js

--- App.js
+++ App.js
@@ -495,34 +495,6 @@ const toggleGroupSelect = (uid) => {
       Alert.alert('Message failed', error.message);
     }
   };
-  
-      const sendGroupChatMessage = async () => {
-  if (!groupMsgDraft.trim() || !activeGroup) return;
-
-  try {
-    await addDoc(
-      collection(
-        db,
-        'groups',
-        activeGroup.id,
-        'messages'
-      ),
-      {
-        text: groupMsgDraft,
-        senderUid: user.uid,
-        senderName: user.name,
-        timestamp: serverTimestamp(),
-      }
-    );
-
-    setGroupMsgDraft('');
-  } catch (error) {
-    Alert.alert(
-      'Message failed',
-      error.message
-    );
-  }
-};
 
 const sendPrivateMessage = async () => {
   if (!privateDraft.trim() || !activePrivateFriend) return;
