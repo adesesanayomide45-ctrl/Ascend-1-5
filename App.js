c8d2cdefb9890634350b46cc4704da779e158530
adesesanayomide45-ctrl <adesesanayomide45@gmail.com>
2026-09-09 20:57:55 +0100

    Update App.js

--- App.js
+++ App.js
@@ -137,6 +137,7 @@ function getBotReply(q) {
   const [profileMenuFor, setProfileMenuFor] = useState(null);const [notifications, setNotifications] = useState([]);
   const [notifOpen, setNotifOpen] = useState(false);
   const [viewProfileFor, setViewProfileFor] = useState(null);
+  const [viewProfileData, setViewProfileData] = useState(null);
 
   const [chats, setChats] = useState([]);
   const [activeChatId, setActiveChatId] = useState(null);
