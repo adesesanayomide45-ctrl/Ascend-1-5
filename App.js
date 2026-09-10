4d318ada3c884830052d49c767848366bfcea2bd
adesesanayomide45-ctrl <adesesanayomide45@gmail.com>
2026-09-10 18:43:28 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Switch, Image, Alert, Modal } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Switch, Image, Alert, Modal , ActivityIndicator } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';import { auth, db, storage } from './firebaseConfig';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { Linking, Share } from 'react-native';
