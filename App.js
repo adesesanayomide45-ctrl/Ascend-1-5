8d98c0e0fa979e17438f86ee777d38b01d042478
adesesanayomide45-ctrl <adesesanayomide45@gmail.com>
2026-09-11 16:50:04 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import * as ImagePicker from 'expo-image-picker';import { auth, db, storage } fr
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { Linking, Share } from 'react-native';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, onAuthStateChanged, signOut } from 'firebase/auth';
-import { collection, addDoc, query, orderBy, onSnapshot, serverTimestamp, doc, setDoc, getDocs, where, updateDoc, deleteDoc, arrayUnion, arrayRemove, increment, getDoc } from 'firebase/firestore';
+import { collection, addDoc, query, orderBy, onSnapshot, serverTimestamp, doc, setDoc, getDocs, where, updateDoc, deleteDoc, arrayUnion, arrayRemove, increment, getDoc, writeBatch } from 'firebase/firestore';
 
 const buildLevels = () => {
   const stages = [
